39be6ac7698b9ac49306fa13026fe485c6ce8081
Dtikoli <tikolidavid@yahoo.com>
2023-02-16 12:32:58 -0800

    Printing output into stderr using write command

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - Entry por
+ * Return: 1
+ */
+int main(void)
+{
+	char str[] = "\"and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	write(2, "\"and that piece of art is useful - Dora Korpar, 2015-10-19\n", strlen(str));
+	return (1);
+}
