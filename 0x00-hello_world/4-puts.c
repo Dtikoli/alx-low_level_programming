15a69e0f9076e511807fbd8acb207746aa2f785c
Dtikoli <tikolidavid@yahoo.com>
2023-02-16 09:18:28 -0800

    Latest on rinting with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * Printing with puts command
  * Main: entry point
  * Return: always 0 (success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
