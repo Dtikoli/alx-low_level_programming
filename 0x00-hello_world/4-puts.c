3dd272d49b8491d3f667ad43e4a288a399ddaaa4
Dtikoli <tikolidavid@yahoo.com>
2023-02-16 08:57:35 -0800

    Printing with pus

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Printing with puts command
+ *
+ * Main: entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
