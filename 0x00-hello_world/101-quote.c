fdda0faf396cd17c0ebb9a1cdff0b902e524e8f4
Dtikoli <tikolidavid@yahoo.com>
2023-02-16 12:52:32 -0800

    Outputing into stderr using write command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 /**
  * main - Entry por
  * Return: 1
@@ -8,7 +9,6 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		strlen(str));
 	return (1);
