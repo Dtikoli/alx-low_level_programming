4972a4391680b3433987ae05f1ab54a7988a7a22
Dtikoli <tikolidavid@yahoo.com>
2023-02-16 09:12:43 -0800

    Revision on rinting with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,12 @@
 
 /**
  * Printing with puts command
- *
  * Main: entry point
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
