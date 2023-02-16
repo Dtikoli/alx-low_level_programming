9ece4ba7716e9488340a930c97569b36ba5c98e0
Dtikoli <tikolidavid@yahoo.com>
2023-02-16 13:17:10 -0800

    Still on writing cutput  into stderr using write command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
  */
 int main(void)
 {
-	int str  = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", str);	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	      strlen(str));
 	return (1);
 }
