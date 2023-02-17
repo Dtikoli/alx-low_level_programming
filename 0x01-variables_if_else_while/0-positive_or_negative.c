db7cdf60c72a513599c665493b485db76caf3545
Dtikoli <tikolidavid@yahoo.com>
2023-02-17 08:34:16 -0800

    Revision to using the if conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,16 +11,14 @@
 int main(void)
 {
 	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n == 0)
 		printf("%i is zero\n", n);
-	else 
+	else
 		printf("%i is negative\n", n);
-	
 	return (0);
 }
