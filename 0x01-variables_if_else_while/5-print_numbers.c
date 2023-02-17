f9a6fc8f4b7c6cd0107828b4b9ab9aa856f165b2
Dtikoli <tikolidavid@yahoo.com>
2023-02-17 11:23:21 -0800

    Printing from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints digits from 0 to 9
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	char ch[]="0123456789";
+	
+	printf("%s\n",ch);
+	return (0);
+}
