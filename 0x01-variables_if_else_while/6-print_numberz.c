1e75f5f71857b698a30e99316c9da7d81442b085
Dtikoli <tikolidavid@yahoo.com>
2023-02-17 11:40:33 -0800

    Alternative of printing numbers from o to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints numbers from 0 to 9
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
