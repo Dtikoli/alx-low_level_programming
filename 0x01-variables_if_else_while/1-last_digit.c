8c630a2d7447dbd39faa480ae7b7a7a8fcd502f6
Dtikoli <tikolidavid@yahoo.com>
2023-02-17 09:16:29 -0800

    Using modular operation to obtain last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	r = n % 10;
 	if (r > 5)
-		printf("The last digit of %i is %i and greater than 5\n", n, r);
+		printf("Last digit of %i is %i and greater than 5\n", n, r);
 	else if (r == 0)
-		printf("The last digit of %i is %i and is 0\n", n, r);
+		printf("Last digit of %i is %i and is 0\n", n, r);
 	else if (r < 6 && r != 0)
-		printf("The last digit of %i is %i and less than 6 and not 0\n", n, r);
+		printf("Last digit of %i is %i and less than 6 and not 0\n", n, r);
 	return (0);
 }
