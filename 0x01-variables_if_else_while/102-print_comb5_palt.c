7a7cf77cf378f6f6ae4a4bf7130fae305b4e5ca4
Dtikoli <tikolidavid@yahoo.com>
2023-02-18 11:42:09 -0800

    Revision of alternate solutions to 101 and 102

--- 0x01-variables_if_else_while/102-print_comb5_palt.c
+++ 0x01-variables_if_else_while/102-print_comb5_palt.c
@@ -11,38 +11,39 @@ int main(void)
 
 	h = '0';
 	while (h <= '9')
+	{
+		g = '0';
+		while (g <= '9')
 		{
-			g = '0';
-			while (g <= '9')
+			e = '0';
+			while (e <= '9')
 			{
-				e = '0';
-				while (e <= '9')
+				i = '0';
+				while (i <= '9')
 				{
-					i = '0';
-					while (i <= '9')
+					op1 = (h * 10) + g;
+					op2 = (e * 10) + i;
+					if (op1 < op2)
 					{
-						op1 = (h * 10) + g;
-						op2 = (e * 10) + i;
-						if (op1 < op2)
-						{
-							putchar(h);
-							putchar(g);
-							putchar(' ');
-							putchar(e);
-							putchar(i);
-					
-							if (!(h == '9' && g == '8' && e == '9' && i == '9'))							
+						putchar(h);
+						putchar(g);
+						putchar(' ');
+						putchar(e);
+						putchar(i);
+						if (!(h == '9' && g == '8' && e == '9' && i == '9'))							
+						{							
 							putchar(',');
 							putchar(' ');
-				 		}
-						i++;
-					}
-					e++;
+						}
+				 	}
+					i++;
 				}
-				g++;
+				e++;
 			}
-			h++;
+			g++;
 		}
+		h++;
+	}
 	putchar('\n');
 	return (0);
 }
