34c0e220f742b1309bba0ce99e27eccc36c468c3
Dtikoli <tikolidavid@yahoo.com>
2023-02-21 06:34:40 -0800

    Printing lowercase alphabet using a self defined function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
