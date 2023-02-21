988a12d4fd1c4339b57fefeddb6873a382c7bdb8
Dtikoli <tikolidavid@yahoo.com>
2023-02-21 06:39:56 -0800

    Using nested while to print alphabet on new lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "mainn.h"
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int c = 1;
+	char ch;
+
+	while (c <= 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		c++;
+	}
+}
