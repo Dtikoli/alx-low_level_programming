cbd07760633c73a5d6cc387ec9a3daf1891ad167
Dtikoli <tikolidavid@yahoo.com>
2023-02-21 07:53:03 -0800

    Printing characters only using ASCII charater code

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,15 +5,10 @@
  * @c: The ASCII code character to check
  * Return: 1 if lower or uppercase, 0 if not alphabet character
  */
-int _isalpha(int ch)
+int _isalpha(int c)
 {
-	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-	{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-	_putchar('\n');
 }
