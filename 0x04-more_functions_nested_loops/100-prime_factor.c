8dbd5ffd093b3eef9efa693ff67f510071ec995d
Dtikoli <tikolidavid@yahoo.com>
2023-02-23 07:46:25 -0800

    Determining prime factors of a number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,12 +9,12 @@ int main(void)
 	long int n, pf;
 
 	n = 612852475143;
-	for (pf = 2; pf <= n; pf++)
+	for (pf = 2; pf < n; pf++)
 	{
 		if (n % pf == 0)
 		{
 			n /= pf;
-			pf--;
+			pf = 2;
 		}
 	}
 	printf("%ld\n", pf);
