889e6dc5426b77064592a389e0cab048dd984ec9
Dtikoli <tikolidavid@yahoo.com>
2023-02-27 10:47:37 -0800

    Latest on printing the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i]; i++)
 		;
 
 	i++;
-	for (i /= 2; str[i] != '\0'; i++)
+	for (i /= 2; str[i]; i++)
 	{
 		_putchar(str[i]);
 	}
