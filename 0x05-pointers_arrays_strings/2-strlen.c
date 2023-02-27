bc0cf7e827950dd2fca4e959bc24ca5c665db5d9
Dtikoli <tikolidavid@yahoo.com>
2023-02-27 07:18:37 -0800

    Finding the length of a strength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of the string
+ * @s: char type pointer
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int ch;
+
+	for (ch = 0; s[ch] != '\0'; ch++)
+		;
+
+	return (ch);
+}
