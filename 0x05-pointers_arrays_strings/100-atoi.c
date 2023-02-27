41e98a86db47231386e8c0e09081e824dff15379
Dtikoli <tikolidavid@yahoo.com>
2023-02-27 11:58:45 -0800

    Converting a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (*s >= 48 && *s <= 57)
 			num = num * 10 + (*s - 48);
-		else if (num > 0)
+		if (s[i + 1] < 48 || s[i + 1] > 57)
 			break;
 		s++;
 	}
