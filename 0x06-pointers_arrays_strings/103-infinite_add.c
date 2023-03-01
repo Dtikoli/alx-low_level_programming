a2aabd87f97b77d8e9afe93f93730111030c159f
Dtikoli <tikolidavid@yahoo.com>
2023-03-01 14:41:08 -0800

    Fixing errors in 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,53 +2,43 @@
 #include <stdio.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void print_buffer(char *b, int size)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int j, k, l;
 
-	while (n1[1])
-		c1++;
-	while (n2[c2])
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
+	if (size <= 0)
+		printf("\n");
 	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = n1[c1] - 48, dr2 = n2[c2] - 48;
-	while (bg >= 0)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-			r[bg] = (op % 10) + 48;
-		else
-			r[bg] = 48;
-		if (c1 > 0)
-			c1--, dr1 = n1[c1] - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = n2[c2] - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
 	}
-	if (*(r) == 48)
-		return (r + 1);
-	else
-		return (r);
 }
+
