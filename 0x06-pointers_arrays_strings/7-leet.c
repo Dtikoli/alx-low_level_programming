04f345a8f161250faa6668cb89aea1a49667500e
Dtikoli <tikolidavid@yahoo.com>
2023-03-01 04:25:55 -0800

    Replacing some letters with numbers in a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encode string into 1337 leet
+ * @s: string to manipulate
+ * Return: string
+ */
+
+char *leet(char *s)
+{
+
+	char decode[]= "aAeEoOtTlL";
+	char encode[] = "4433007711";
+
+	int i, j;
+
+	for (j = 0; s[j]; j++)
+	{
+		for (i = 0; decode[i]; i++)
+		{
+			if (s[j] == decode[i])
+				s[j] = encode[i];
+		}
+	}
+
+	return (s);
+}
+
