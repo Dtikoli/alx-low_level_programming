3f7fcb63c90c1cba15b19002fa2e83c0d02ced32
Dtikoli <tikolidavid@yahoo.com>
2023-03-01 14:38:03 -0800

    Fixing errors in 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -32,7 +32,7 @@ void print_buffer(char *b, int size)
 			{
 				if (l >= size)
 					break;
-				if (b[l]) < 32 || b[l] > 126)
+				if (b[l] < 32 || b[l] > 126)
 					printf("%c", '.');
 				else
 					printf("%c", b[l]);
