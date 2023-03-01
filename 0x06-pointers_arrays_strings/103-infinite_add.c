d1ec0928c487f272511cd226cead2175463de545
Dtikoli <tikolidavid@yahoo.com>
2023-03-01 14:48:12 -0800

    Fixing errors in 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -23,7 +23,7 @@ void print_buffer(char *b, int size)
 				if (k % 2 == 0)
 					printf(" ");
 				if (k < size)
-					printf("%.2x", *(b + k));
+					printf("%.2x", b[k];
 				else
 					printf("  ");
 			}
@@ -32,10 +32,10 @@ void print_buffer(char *b, int size)
 			{
 				if (l >= size)
 					break;
-				if (*(b + l) < 32 || *(b + l) > 126)
+				if (b[l] < 32 || b[l] > 126)
 					printf("%c", '.');
 				else
-					printf("%c", *(b + l));
+					printf("%c", b[l]);
 			}
 			printf("\n");
 		}
