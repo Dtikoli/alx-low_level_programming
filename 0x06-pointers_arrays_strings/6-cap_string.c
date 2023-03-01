33ea9a8bdb92812fac19864515280449f395db23
Dtikoli <tikolidavid@yahoo.com>
2023-03-01 03:38:42 -0800

    Capitalising the words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#define DIFF 32
+
+/**
+ * cap_string - capitalize first letter of each word
+ * @s: string to manipulate
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	/* check first index for capital */
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= DIFF;
+	i++;
+
+	while (s[i]) /* iterate through string */
+	{
+
+		/* if lowercase and preceded by separator, capitalize*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] -= DIFF;
+		i++;
+	}
+
+	return (s);
+}
