15a2b6282d840169054f8bfaff1713d5bcc87d90
Dtikoli <tikolidavid@yahoo.com>
2023-03-01 10:57:03 -0800

    Comparing the  contents of a string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] &&  s2[i] && s1[i] == s2[i]; i++)
+	for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++)
 		;
 	return (s1[i] - s2[i]);
 }
