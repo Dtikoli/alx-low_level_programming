b20cbde73a4f5a2e6a9e4df31d097e22bbbd5512
Dtikoli <tikolidavid@yahoo.com>
2023-03-03 23:26:48 -0800

    Latest on locating a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,31 @@
 #include "main.h"
 
 /**
- * _strstr - first occurrence of the substring needle in the string haystack
- * @haystack: main str to be examined
- * @needle: searched in haystack
- * Return: return 0
- **/
-char  *_strstr(char *haystack, char *needle)
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
 {
-        char *s1, *s2;
+	char *bhaystack;
+	char *pneedle;
 
-        while (*haystack)
-        {
-                if(*haystack == *needle)
-                {
-                        s1 = haystack;
-                        s2 = needle;
-                        while (*s1 && *s2)
-                        {
-                                if(*s1 != *s2)
-                                        break;
-                                s1++;
-                                s2++;
-                        }
-                        if(*s2 == '\0')
-                                return (haystack);
-                }
-                haystack++;
-        }
-        return (0);
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
 }
