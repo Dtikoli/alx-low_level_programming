90b66c33baca34c4a8e6b87f0d140c756c8e7026
Dtikoli <tikolidavid@yahoo.com>
2023-03-03 23:30:29 -0800

    Latest revision to creating chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * print_chessboard - print chessboard given set 2D array
- * @a: 2D array
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
  */
+
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int y, x;
 
-	for (i = 0; 1 < 8; i++)
+	y = 0;
+	while (y < 8)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
+		x = 0;
+		while (x < 8)
+		{
+			_putchar(a[y][x]);
+			x++;
+		}
 		_putchar('\n');
+		y++;
 	}
 }
