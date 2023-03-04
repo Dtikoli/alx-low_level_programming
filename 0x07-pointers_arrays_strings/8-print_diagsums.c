116e69fabcfeef2596f35fce09ac802c659648d7
Dtikoli <tikolidavid@yahoo.com>
2023-03-03 23:33:32 -0800

    Revision to summing the diagonals of a 2D array

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,21 +6,26 @@
  * @a: matrix
  * @size: size of matrix
  */
+
 void print_diagsums(int *a, int size)
 {
-        int diag_sum1 = 0;
-        int diag_sum2 = 0;
-        int row, i;
 
-        for (row = 0; row < size; row++)
-        {
-                i = (row * size) + row;
-                diag_sum1 += a[i];
-        }
-        for (row = 1; row <= size; row++)
-        {
-                i = (row * size) - row;
-                diag_sum2 += a[i];
-        }
-        printf("%d, %d\n", diag_sum1, diag_sum2);
+	int diagonal_sum_1 = 0;
+	int diagonal_sum_2 = 0;
+	int row, i;
+
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		diagonal_sum_1 += a[i];
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		diagonal_sum_2 += a[i];
+	}
+
+	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
+
 }
