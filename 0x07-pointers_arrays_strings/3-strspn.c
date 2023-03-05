7ffb66de330cc3033890b016b85d6af7dba49f22
Dtikoli <tikolidavid@yahoo.com>
2023-03-04 23:59:48 -0800

    Most Recent on finding the length of a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,27 +6,19 @@
  * @accept: target matches
  * Return: number of bytes consecutively matched
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j;
+	int i, j;
 	int c = 0;
 
-	while (s[i] != '\0') /*iterate through string*/
+	for (i = 0; s[i]; i++) /*iterate through string*/
 	{
-
-		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
-		{
-			if (s[i] == accept[j]) /*record & break at first match*/
-			{
-				c++;
-				break;
-			}
-			if (accept[j + 1] == '\0' && s[i] != accept[j])
-				return (c);/*return if idx doesn't match*/
-		}
-		i++;
+		for (j = 0; accept[j] && s[i] != accept[j]; j++) /*find a match*/
+			;
+		if (s[i] == accept[j]) /*record at first match*/
+			c++;
+		else
+			return (c); /*return record if next i doesn't match*/
 	}
-	return (c); /* return num if all match till end */
-
+	return (c); /* return num of all match till end */
 }
