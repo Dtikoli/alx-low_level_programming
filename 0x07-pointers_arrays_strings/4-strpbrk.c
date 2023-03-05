244c6d605492897cb1f24f4102778972fcab8523
Dtikoli <tikolidavid@yahoo.com>
2023-03-05 00:03:11 -0800

    Most recent on searching a string for any set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define NULL 0
 
 /**
  * _strpbrk - return pointer to byte in s that matches a byte in accept
@@ -7,24 +6,16 @@
  * @accept: target matches
  * Return: pointer to index of string at first occurence
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j;
+	int i, j;
 
-	while (s[i] != '\0') /*iterate through string*/
+	for (i = 0; s[i]; i++)
 	{
-
-		for (j = 0; accept[j] != '\0'; j++) /* iterate through target */
-		{
-			if (s[i] == accept[j]) /* stop at first match */
-			{
-				s = &s[i]; /* set pointer to first occurence */
-				return (s);
-			}
-		}
-		i++;
+		for (j = 0; accept[j] && s[i] != accept[j]; j++)
+			;
+		if (s[i] == accept[j])
+			return (&s[i]);
 	}
-	return (NULL); /* return NULL if no matches */
-
+	return (0);
 }
