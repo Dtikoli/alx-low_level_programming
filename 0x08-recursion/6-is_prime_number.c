00c38d7bcf869cafbb8d8714252a9967158ff25b
Dtikoli <tikolidavid@yahoo.com>
2023-03-08 04:09:22 -0800

    Prime numbers with recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * prime_help - helper function for is_prime_number
+ * @n: number
+ * @j: incrementer
+ *
+ * Return: 1 if n is a prime number, otherwise 0
+ */
+int prime_help(int n, int j)
+{
+	if (j == n)
+		return (1);
+	if (n % j == 0)
+		return (0);
+	return (prime_help(n, j + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ * @n: number
+ *
+ * Return: 1 if n is a prime number, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime_help(n, 2));
+}
