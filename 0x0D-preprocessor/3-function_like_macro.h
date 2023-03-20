4c77d7f53b51a59dec50756ddc58743c9260b95b
Dtikoli <tikolidavid@yahoo.com>
2023-03-20 04:10:55 -0700

    Header files with an absolute number macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_
 #define _FUNCTION_LIKE_MACRO_
 
-#define ABS(x) ((x) > (0) ? (x) : (-x))
+#define ABS(x) ((x) > 0 ? (x) : (-x))
 
 #endif /* #ifndef _FUNCTION_LIKE_MACRO_ */
