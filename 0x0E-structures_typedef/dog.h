ada11ca2a75fc62a3b0f3f2e5d17ad926c47c35d
Dtikoli <tikolidavid@yahoo.com>
2023-03-20 10:59:01 -0700

    Declaring a data structure in a header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - structure definition of a dog
- * @name: character string
- * @age: integer
- * @owner: character string
+ * @name: character string representing name of dog
+ * @age: float representing age of dog
+ * @owner: character string representing owner of dog
  */
 typedef struct dog
 {
@@ -18,4 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /* #ifndef _DOG_H_ */
