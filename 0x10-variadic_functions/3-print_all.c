a9c7388d35467e38bb4e4070c0819a7378004826
Dtikoli <tikolidavid@yahoo.com>
2023-03-23 00:40:12 -0700

    Printing any data type using variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,78 @@
+#include "variadic_functions.h"
+
+/**
+ * _printchar - print char type element from va_list
+ * @list: va_list passed to function
+ */
+void _printchar(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * _printstr - print string element from va_list
+ * @list: va_list passed to function
+ */
+void _printstr(va_list list)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+ * _printfloat - print float type element from va_list
+ * @list: va_list passed to function
+ */
+void _printfloat(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * _printint - print int type element from va_list
+ * @list: va_list passed to function
+ */
+void _printint(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_all - print anything passed if char, int, float, or string.
+ * @format: string of formats to use and print
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	va_list args;
+	char *delim;
+
+	_dt list[] = {
+		{ "c", _printchar },
+		{ "f", _printfloat },
+		{ "s", _printstr },
+		{ "i", _printint },
+		{ NULL, NULL }
+	};
+
+	i = 0;
+	delim = "";
+	va_start(args, format);
+	while (format != NULL && format[i / 4] != '\0')
+	{
+		j = i % 4;
+		if (list[j].type[0] == format[i / 4])
+		{
+			printf("%s", delim);
+			list[j].f(args);
+			delim = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
