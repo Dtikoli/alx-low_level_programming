c49918f3fdf010e51e268af025858e34a0bd15fe
Dtikoli <tikolidavid@yahoo.com>
2023-04-05 09:55:28 -0700

    Alternatnate to safe printing

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,65 +1,80 @@
 #include "lists.h"
 
 /**
- * _free_listp - frees a linked list
- * @head: head pointer.
- * Return: no return.
+ * _loop_listint_len - finds the length of a loop in a linked list
+ * @head: linked list
+ * Return: the number of nodes in the loop, or 0 if there is no loop
  */
-void _free_listp(listp_t **head)
+size_t _loop_listint_len(const listint_t *head)
 {
-	listp_t *tmp;
+	const listint_t *lag, *lead;
+	size_t c = 1;
 
-	if (head == NULL)
-		return;
+	/* If the list is empty or has only one node, there is no loop */
+	if (head == NULL || head->next == NULL)
+		return (0);
 
-	while (*head)
+	lag = lead = head;
+
+	/* Floyd's cycle-finding algorithm */
+	while (lag && lead && lead->next)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		lag = lag->next;
+		lead = lead->next->next;
+		if (lag == lead)
+		{
+			lag = head;
+			while (lag != lead)
+			{
+				c++;
+				lag = lag->next;
+				lead = lead->next;
+			}
+
+			lag = lag->next;
+			while (lag != lead)
+			{
+				c++;
+				lag = lag->next;
+			}
+
+			return (c);
+		}
+
 	}
+
+	return (0);
 }
 
 /**
- * print_listint_safe - prints a linked list.
+ * print_listint_safe - Prints the elements in a linked list safely.
  * @head: linked list.
- * Return: number of nodes in the list.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t c;
-	listp_t *hpt, *prev, *nex;
+	size_t c, i;
 
-	hpt = NULL;
-	for (c = 0; head; c++)
+	c = _loop_listint_len(head);
+
+	if (!c) /* same as if c = 0 */
 	{
-		prev = malloc(sizeof(listp_t));
-
-		if (prev == NULL)
-			exit(98);
-
-		prev->p = (void *)head;
-		prev->next = hpt;
-		hpt = prev;
-
-		nex = hpt;
-
-		while (nex->next)
+		for (; head; c++)
 		{
-			nex = nex->next;
-			if (head == nex->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				_free_listp(&hpt);
-				return (c);
-			}
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next;
 		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+	}
+	else
+	{
+		for (i = 0; i < c; i++)
+		{
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *) head, head->n);
 	}
 
-	_free_listp(&hpt);
 	return (c);
 }
 
