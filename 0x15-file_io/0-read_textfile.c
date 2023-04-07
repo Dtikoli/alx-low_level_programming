ab0c61a88d3d65842fe111da1adb208e04f07333
Dtikoli <tikolidavid@yahoo.com>
2023-04-07 10:37:15 -0700

    Reading and printing a text file to STDOUT

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,24 +16,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 
 	r_count = read(fd, buffer, letters);
-	w_count = write(STDOUT_FILENO, buffer, r_count);
-
-	if (w_count != r_count || w_count == -1)
+	if (r_count == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 
-	close(fd);
+	w_count = write(STDOUT_FILENO, buffer, r_count);
+	if (w_count != r_count || w_count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
 	free(buffer);
-
+	close(fd);
 	return (w_count);
 }
 
