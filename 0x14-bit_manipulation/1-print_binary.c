6dcc2576e1c8ec73ef5ee66b2553550372da295f
Dtikoli <tikolidavid@yahoo.com>
2023-04-07 09:04:47 -0700

    Printing a number in binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_binary - prints an integer if a binary format
+ * print_binary - prints an integer in a binary format
  * @n: unsigned long integer.
  * Return: nothing.
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
-	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else
+	if (!(n > 0))
 	{
 		_putchar('0');
+	}
+
+	else
+	{
+		if (n > 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 }
