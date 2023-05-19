88dc31824e8185991703456528a8e86a4f179a18
Dtikoli <tikolidavid@yahoo.com>
2023-05-19 04:12:16 -0700

    Finds the largest palindrome made from the product of two 3-digit numbers

--- 0x17-doubly_linked_lists/102-palindromes.py
+++ 0x17-doubly_linked_lists/102-palindromes.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-
 def is_Palin(mul):
-    if str(mul) == "".join(reversed(str(mul))):
-        return True
-    else:
-        return False
-
-# Note: str(mul)[-1::-1]) == str(mul)[::-1] == "".join(reversed(str(mul)))
-# You can simpy: return (str(mul) == str(mul)[::-1])
+    return (str(mul) == str(mul)[::-1])
 
 
 def seek_largest_palindrome():
-    large_Pal = 0
-    for i in range(999, 99, -1):
-        for j in range(999, 99, -1):
-            mul = i * j
-            if (is_Palin(mul)) and mul > large_Pal:
-                large_Pal = mul
-    return(large_Pal)
+    return max([i * j for i in range(999, 99, -1) for j in range(999, 99, -1)
+               if is_Palin(i * j)])
 
 
 print(seek_largest_palindrome())
